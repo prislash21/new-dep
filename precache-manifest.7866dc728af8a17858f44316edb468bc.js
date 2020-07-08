830436f328bcfc10452847041ec606496b6e63a6
prislash21 <prislash21@gmail.com>
2020-07-09 00:51:51 +0600

    Updates

---
+++ precache-manifest.7866dc728af8a17858f44316edb468bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af1e51965f87cc2eec530ce89fba8bf0",
+    "url": "/new-dep/index.html"
+  },
+  {
+    "revision": "23953ff9e2c51b9bdb4f",
+    "url": "/new-dep/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f0dbd56e7c6a51c6cd3",
+    "url": "/new-dep/static/js/2.d02d8524.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-dep/static/js/2.d02d8524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23953ff9e2c51b9bdb4f",
+    "url": "/new-dep/static/js/main.d181d653.chunk.js"
+  },
+  {
+    "revision": "83e47eddf73dd0a12d4a",
+    "url": "/new-dep/static/js/runtime-main.7d66221b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-dep/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae0490786bfacd52037d17033d2d0c3d.js"
+  "/new-dep/precache-manifest.7866dc728af8a17858f44316edb468bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/new-dep/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
